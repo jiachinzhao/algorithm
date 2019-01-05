ed057e29c41f4b911bf8c40a91302e367f8e5d5f
jiachinzhao <jiachinzhao@qq.com>
2019-01-05 23:40:29 +0800

    dp min edit distance

--- lettcode_72/main.go
+++ lettcode_72/main.go
@@ -2,44 +2,59 @@ package main
 
 import "fmt"
 
-func uniquePathsWithObstacles(obstacleGrid [][]int) int {
-	n := len(obstacleGrid)
-	if n == 0{
-		return 0
-	}
-	m := len(obstacleGrid[0])
-	if m == 0{
-		return 0
-	}
-	dp := make([][]int, n)
-	for i  := range obstacleGrid{
-		dp[i] = make([]int, m)
-	}
-	dp[0][0] = 1 - obstacleGrid[0][0]
+func main() {
+	fmt.Println(minDistance("horse", "ros"))
+	fmt.Println(minDistance("intention", "execution"))
+	fmt.Println(minDistance("", "a"))
+	fmt.Println(minDistance("a", "ab"))
+}
 
-	for i := 0;i < n;i++{
-		for j := 0; j < m;j++{
-			if i == 0 && j == 0{
-				continue
-			}
-			if obstacleGrid[i][j] == 0{
-				if i > 0{
-					dp[i][j] += dp[i - 1][j]
+//we define dp[i][j] is -min distance -> w1[...i] and w2[...j] match each other,
+//we consider w1[i] and w2[j]
+//if w1[i] = w2[j], it can change to dp[i - 1][j - 1]
+//otherwise, we can use replace or insert(equal to delete) change to {dp[i-1][j-1], dp[i-1][j], dp[i][j - 1]}
+
+func minDistance(word1 string, word2 string) int {
+	if len(word2) > len(word1) {
+		word1, word2 = word2, word1
+	}
+	if len(word2) == 0 {
+		return len(word1)
+	}
+	min := func(x, y int) int {
+		if x < y {
+			return x
+		}
+		return y
+	}
+
+	dp := make([][]int, len(word1))
+	for i := range word1 {
+		dp[i] = make([]int, len(word2))
+	}
+
+	for i, w1 := range word1 {
+		for j, w2 := range word2 {
+			if w1 == w2 {
+				if i == 0 {
+					dp[i][j] = j
+				} else if j == 0 {
+					dp[i][j] = i
+				} else {
+					dp[i][j] = dp[i-1][j-1]
 				}
-				if j > 0{
-					dp[i][j] += dp[i][j - 1]
+			} else {
+				if i == 0 && j == 0 {
+					dp[i][j] = 1
+				} else if j == 0 {
+					dp[i][j] = dp[i-1][j] + 1
+				} else if i == 0 {
+					dp[i][j] = dp[i][j-1] + 1
+				} else {
+					dp[i][j] = min(min(dp[i-1][j], dp[i][j-1]), dp[i-1][j-1]) + 1
 				}
 			}
 		}
 	}
-	return dp[n - 1][m - 1]
-
+	return dp[len(word1)-1][len(word2)-1]
 }
-func main(){
-	sample1 := [][]int{
-		[]int{0,0,0}, []int{0,1,0},[]int{0,0,0},
-	}
-	fmt.Println(uniquePathsWithObstacles(sample1))
-
-}
-
